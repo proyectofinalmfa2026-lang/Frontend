4900ecd7e29071cdc0c02458202353c554e397d7
MorningstarDev <matdourodriguez@gmail.com>
2026-06-11 19:32:12 -0300

    fix: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  allowedDevOrigins: [
+    '192.168.1.40',
+  ],
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
+
